aa1ec34775e225709378cd4546b346dc3ab19f00
Angel Taipe <angeltaipe364@gmail.com>
2025-01-29 19:33:12 -0500

    Cambio 1

---
+++ AngelTaipe.py
@@ -0,0 +1,20 @@
+# Definición de la Multiplicación
+
+def multiplicar(a, b):
+    return a * b
+
+# Ejemplo de uso
+resultado = multiplicar(4, 5)
+print(resultado)  # Salida: 20
+
+
+
+# Modifico el archivo series.py y coloco otra funcion
+#   Definición de la Divición 
+
+def dividir(a, b):
+    return a / b
+
+# Ejemplo de uso
+resultado = dividir(30, 5)
+print(resultado)  # Salida: 6.0
